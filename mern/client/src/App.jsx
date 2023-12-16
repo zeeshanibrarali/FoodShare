876ec5212d052eb1138b33d92c4288ac031400a6
zeeshanibrarali <zeeshanibrar10987@gmail.com>
2023-12-16 16:15:27 +0500

    render home page

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { useState } from 'react';
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {  } from "@fortawesome/free-solid-svg-icons";
 import Header from "./Components/header";
 import Info from "./Components/info";
 import WhatWeDo from "./Components/WhatWeDo";
 import Footer from "./Components/footer";
 import SponsorLogo from "./Components/sponsorLogo";
-import Dashboard from "./Components/DashBoard/dashboard";
-import Sidebar from "./Components/DashBoard/sidebar";
+// import Dashboard from "./Components/DashBoard/dashboard";
+// import Sidebar from "./Components/DashBoard/sidebar";
 // import Login from "./Components/LoginForm/login";
 // import Login from "./Components/login";
-import './App.css'
-
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <> 
-      {/* <Header />
+      <Header />
       <WhatWeDo />
       <Info />
       <SponsorLogo />
-      <Footer /> */}
-      <Sidebar /> 
-      <Dashboard />
-
+      <Footer />
+      {/* <Sidebar />  */}
+      {/* <Dashboard /> */}
       {/* <Login /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- mern/server/server.mjs
+++ mern/server/server.mjs
@@ -1,17 +1,29 @@
 import express from "express";
 import cors from "cors";
+import { fileURLToPath } from 'url';
+import path from "path";
 import "./loadEnvironment.mjs";
-import records from "./routes/record.mjs";
+// import records from "./routes/record.mjs";
+import homeRoute from "./routes/homeRoute.mjs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 5050;
 const app = express();
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../client")));
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/record", records);
+// app.use("/record", records);
 
-// start the Express server
+// Use the home route for the root path ("/")
+app.use("/", homeRoute);
+
+// Start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
